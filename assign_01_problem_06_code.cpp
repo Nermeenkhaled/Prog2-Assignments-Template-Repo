692e074a4c4c32c7e38318f685cea8e79e2cf63e
Nermeenkhaled <128297015+Nermeenkhaled@users.noreply.github.com>
2023-04-08 21:49:28 +0200

    Rename assign_01_problem_06_code.txt to assign_01_problem_06_code.cpp

---
+++ assign_01_problem_06_code.cpp
@@ -0,0 +1,28 @@
+//Nermeen Khaled 20216111
+
+#include <iostream>
+
+
+using namespace std;
+
+
+int main()
+
+
+{ 
+   
+int x ;
+   
+cin >> x ; 
+   
+int result = x/1000 ;
+   
+if ( result % 2 == 0) 
+   
+  cout << "EVEN";
+ 
+else 
+ 
+  cout <<"ODD";
+
+}
